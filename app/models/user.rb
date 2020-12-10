a7aeae7e867336d7e63baa2aeb69c7c022f15eb8
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-12-10 16:09:05 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, password_length: 10..128
 
-  validates :name, presence: true
+  validates :name, presence: true,
+  uniqueness: { case_sensitive: :false },
+  length: { minimum: 2, maximum: 20 }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  EMAIL_REGEX = /.....+@(?=staherqui)/.freeze
+  validates_format_of :email, with: EMAIL_REGEX
+
 end
