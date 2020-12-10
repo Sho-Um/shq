ea3c0fcfd662e12e9bd81dc7bb43224d81036f81
Sho-Um <zillion.of.zest.stay.gold.sho1@gmail.com>
2020-12-10 16:40:27 +0900

    インスタンスの並び順変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def show
-    @questions = current_user.questions
+    @questions = current_user.questions.order("created_at DESC")
   end
 
 end
